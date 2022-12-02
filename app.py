9745cbca4a7a16dcce125355bbff99a874af01b0
roxletst <roxana_steinman@hotmail.com>
2022-12-02 20:06:37 -0300

    clase 021222

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
-
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
+mysql = MySQL()
+app.config ['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config ['MYSQL_DATABASE_USER'] = 'root'
+app.config ['MYSQL_DATABASE_PASSWORD'] = ''
+app.config ['MYSQL_DATABASE_DB'] = 'sistema22515'
+mysql.init_app(app)
+
 @app.route("/")
 def index():
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Messi', 'messi@gmail.com', 'fotodemessi.jpg');"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
     return render_template('empleados/index.html')
 
 
