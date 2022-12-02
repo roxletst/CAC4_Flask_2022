1b03cf84d0a8d90be6702f21b2eb8378ea636161
roxletst <roxana_steinman@hotmail.com>
2022-12-02 20:51:33 -0300

    test BD con foto fechada

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,6 +21,29 @@ def index():
     conn.commit()
     return render_template('empleados/index.html')
 
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+@app.route('/store', methods = ['POST'])
+def storage():
+    _nombre = request.form['txtNombre']
+    _correo = request.form['txtCorreo']
+    _foto = request.files['txtFoto']
+
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    if _foto.filename !='':
+        nuevoNombreFoto = tiempo + _foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
+    datos = (_nombre, _correo, nuevoNombreFoto)
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
